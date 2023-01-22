285ed672a38822b1c7b475acd99623d72d5227ef
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-01-22 16:31:20 +0700

    Remove Unnecessary

--- src/template/Template.js
+++ src/template/Template.js
@@ -21,22 +21,7 @@ class Template extends PureComponent {
                     {this.props.children}
                     <Footer/>
                 </div>
-
-                <script src="//static.zdassets.com/hc/assets/en-us.2c5c638c28926554a2d6.js"/>
-                <script
-                    src="https://kalibrr-support.zendesk.com/auth/v2/host.js"
-                    data-brand-id="114095008172"
-                    data-return-to="https://kalibrr-support.zendesk.com/hc/en-us"
-                    data-theme="hc"
-                    data-locale="en-us"
-                    data-auth-origin="114095008172,false,true"
-                />
-                <script src="//static.zdassets.com/hc/assets/hc_enduser-fc5f459435153cda7be0d2b49736899f.js"/>
-                <script src="//static.zdassets.com/hc/assets/hc_analytics-c03f86cfac0a35d7f97a26b90615fd00.js"/>
-                <script
-                    type="text/javascript"
-                    src="//p25.zdassets.com/hc/theming_assets/2132548/114095008172/script.js?digest=8637358424857"
-                />
+                
                 <script
                     defer
                     src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993"
